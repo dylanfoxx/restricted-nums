3bb2134225461b0f1cb81cf88e9bebae0b1088d2
Dylan Gebel <dylanfoxx@users.noreply.github.com>
2021-05-31 16:23:54 -0400

    Clean up, add as_ref

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,28 @@
+#![no_std]
+
 // Things to add.
 // - Checking for over or underflow in operations Ex: <_, i8::MAX>::new_max() + 1; I think is currently in range but should not be ... maybe?
-// - More useful traits
 // - Documentation
 // - Fill out tests
-// - move everything to core so we can be no_std
 // - Idents may be weird, is there a way to do this with types
 // - Add serde feature
 
-//! restricted-nums contains all core num types(not floats) but in restricted form. Meaning they can be limited by const values on deceleration. 
+//! restricted-nums contains all core num types(not floats) but in restricted form. Meaning they can be limited by const values on deceleration.
+//!
+//! Due to this most operations return Option<Self>, with only a few that are guaranteed to succeed returning Self.
 //! ```
-//!     let mynum: Rnumi8 = RNumi8::<1, 20>::new_min(); //Is 1
-//!     let mynum2: Option<RNumi32> = RNumi32::<500, 20000>::new(400); //Is None
+//!     use restricted_nums::{RNumi8, RNumi32};
+//!
+//!     let my_num = RNumi8::<1, 20>::new_min(); //Is 1
+//!     let complicated_num: i32 = -5;
+//!     let new_value = my_num + complicated_num;
+//!     assert!(new_value == None);
+//!
+//!     fn calc(value : impl AsRef<i8>){
+//!         print!("{}", value.as_ref());
+//!     }
+//!
+//!     calc(my_num);
 //! ```
 //! Most useful traits have been implemented on each type.
 //! Each trait should be what you think but double check the documentation of each function to confirm. Ex: 
@@ -18,14 +30,14 @@
 // We use paste to concat idents. There may be a better way to do this.
 use paste::paste;
 
-use std::convert::TryFrom;
+// May be worth changing the Error type here
+use core::convert::TryFrom;
 macro_rules! auto_struct_try_from {
     ($cur:ident, $($t:ident)*) => {
         paste! { $(
             impl<const MIN: $cur, const MAX: $cur> TryFrom<$t> for [<RNum $cur>]<MIN, MAX> {
                 type Error = &'static str;
 
-                #[inline]
                 fn try_from(value: $t) -> Result<Self, Self::Error> {
                     if let Ok(new_val) = $cur::try_from(value) {
                         if Self::in_range(new_val){
@@ -39,12 +51,17 @@ macro_rules! auto_struct_try_from {
 
         #[test]
         fn [<rnum $cur _fail_from>]() {
-
+            let value = [<RNum $cur>]::<1, 120>::try_from(240 + 1);
+            assert!(value.ok() == None);
+            let value = [<RNum $cur>]::<1, 120>::try_from(0);
+            assert!(value.ok() == None);
+            let value = [<RNum $cur>]::<1, 120>::try_from(7);
+            assert!(value.ok().is_some());
         }}
     };
 }
 
-use std::cmp::Ordering;
+use core::cmp::Ordering;
 macro_rules! auto_struct_partial_cmp {
     ($cur:ident, $($t:ident)*) => {
         paste! { $(
@@ -78,14 +95,13 @@ macro_rules! auto_struct_partial_cmp {
     };
 }
 
-use std::ops::Add;
+use core::ops::Add;
 macro_rules! auto_struct_add {
     ($cur:ident, $($t:ident)*) => {
         paste! { $(
             impl<const MIN: $cur, const MAX: $cur> Add<$t> for [<RNum $cur>]<MIN, MAX> {
                 type Output = Option<Self>;
 
-                #[inline]
                 fn add(self, other: $t) -> Self::Output {
                     if let Ok(new_other) = Self::try_from(other){
                         // Change this to an add i can check
@@ -104,14 +120,13 @@ macro_rules! auto_struct_add {
     };
 }
 
-use std::ops::Mul;
+use core::ops::Mul;
 macro_rules! auto_struct_mul {
     ($cur:ident, $($t:ident)*) => {
         paste! { $(
             impl<const MIN: $cur, const MAX: $cur> Mul<$t> for [<RNum $cur>]<MIN, MAX> {
                 type Output = Option<Self>;
 
-                #[inline]
                 fn mul(self, other: $t) -> Self::Output {
                     if let Ok(new_other) = Self::try_from(other){
                         // Change this to an add i can check
@@ -130,14 +145,13 @@ macro_rules! auto_struct_mul {
     };
 }
 
-use std::ops::Div;
+use core::ops::Div;
 macro_rules! auto_struct_div {
     ($cur:ident, $($t:ident)*) => {
         paste! { $(
             impl<const MIN: $cur, const MAX: $cur> Div<$t> for [<RNum $cur>]<MIN, MAX> {
                 type Output = Option<Self>;
 
-                #[inline]
                 fn div(self, other: $t) -> Self::Output {
                     if let Ok(new_other) = Self::try_from(other){
                         // Change this to an add i can check
@@ -156,9 +170,10 @@ macro_rules! auto_struct_div {
     };
 }
 
+use core::convert::AsRef;
 use const_fn_assert::cfn_assert;
-use std::ops::Deref;
-use std::fmt::{self, Display};
+use core::ops::Deref;
+use core::fmt::{self, Display};
 macro_rules! auto_struct {
     ($($t:ident)*) => {
         paste! { $(
@@ -187,17 +202,18 @@ macro_rules! auto_struct {
 
                 ///# Safety
                 /// This will not check to make sure your value is within [MIN, MAX]
-                ///
                 pub const unsafe fn new_uncheck(value: $t) ->Self {
                     cfn_assert!(MIN < MAX);
                     Self{value}
                 }
 
                 pub const fn new_min() -> Self{
+                    cfn_assert!(MIN < MAX);
                     Self{value: MIN}
                 }
 
                 pub const fn new_max() -> Self{
+                    cfn_assert!(MIN < MAX);
                     Self{value: MIN}
                 }
             }
@@ -238,7 +254,6 @@ macro_rules! auto_struct {
             impl<const MIN: $t, const MAX: $t> Add for [<RNum $t>]<MIN, MAX> {
                 type Output = Option<Self>;
 
-                #[inline]
                 fn add(self, other: Self) -> Self::Output {
                     let new = self.value + other.value;
                     Self::try_from(new).ok()
@@ -248,7 +263,6 @@ macro_rules! auto_struct {
             impl<const MIN: $t, const MAX: $t> Mul for [<RNum $t>]<MIN, MAX> {
                 type Output = Option<Self>;
 
-                #[inline]
                 fn mul(self, other: Self) -> Self::Output {
                     let new = self.value * other.value;
                     Self::try_from(new).ok()
@@ -258,10 +272,17 @@ macro_rules! auto_struct {
             impl<const MIN: $t, const MAX: $t> Div for [<RNum $t>]<MIN, MAX> {
                 type Output = Option<Self>;
 
-                #[inline]
                 fn div(self, other: Self) -> Self::Output {
                     let new = self.value / other.value;
                     Self::try_from(new).ok()
+                }
+            }
+
+            impl<const MIN: $t, const MAX: $t> AsRef<$t> for [<RNum $t>]<MIN, MAX> {
+
+                #[inline]
+                fn as_ref(&self) -> &$t {
+                    &self.value
                 }
             }
 
